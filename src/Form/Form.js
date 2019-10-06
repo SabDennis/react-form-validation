aff65f2008457d65b9c6f722880389445ec83631
Dennis Sabinin <sawyer7071@gmail.com>
2019-10-06 14:24:35 +0300

    added checkbox

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -3,20 +3,36 @@ import React from "react";
 class Form extends React.Component {
 
     state = {
-        firstName : '',
-        lastName : '',
-        email : '',
-        telNumber : '',
-        continent : '',
+        firstName: '',
+        lastName: '',
+        email: '',
+        phoneNumber: '',
+        continent: '',
         sex: '',
-        interests : {
-            sport : false,
-            coding : false,
-            videogames : false, 
-            art : false, 
-            music : false
+        activities: {
+            sport: false,
+            coding: false,
+            videogames: false,
+            art: false,
+            music: false
         },
-        date : ''
+        date: ''
+    };
+
+    handleChange = event => {
+        const isCheckbox = event.target.type === 'checkbox';
+
+        if (isCheckbox) {
+            const activities = this.state.activities;
+            for (let activity in activities) {
+                if (activity === event.target.value) {
+                    activities[activity] = event.target.checked;
+                }
+            }
+            this.setState({ activities: activities });
+        } else {
+            this.setState({ [event.target.name]: event.target.value });
+        }
     };
 
     handleSubmit = event => {
@@ -30,76 +46,106 @@ class Form extends React.Component {
                 <div>
                     <label>
                         <input
+                            name="firstName"
+                            value={this.state.firstName}
+                            onChange={this.handleChange}
                             placeholder="First Name" />
                     </label>
                     <label>
                         <input
+                            name="lastName"
+                            value={this.state.lastName}
+                            onChange={this.handleChange}
                             placeholder="Last Name" />
                     </label>
                 </div>
                 <div>
                     <label>
                         <input
+                            name="email"
+                            value={this.state.email}
+                            onChange={this.handleChange}
                             placeholder="Email" />
                     </label>
                     <label>
                         <input
+                            name="phoneNumber"
+                            value={this.state.phoneNumber}
+                            onChange={this.handleChange}
                             placeholder="Phone Number" />
                     </label>
                 </div>
-                <select>
-                    <option>Eurasia</option>
-                    <option>Africa</option>
-                    <option>North America</option>
-                    <option>South America</option>
-                    <option>Australia</option>
-                    <option>Antarctica</option>
+                <select
+                    name='continent'
+                    value={this.state.continent}
+                    onChange={this.handleChange}>
+                    <option value="Eurasia">Eurasia</option>
+                    <option value="Africa">Africa</option>
+                    <option value="America">North America</option>
+                    <option value="South America">South America</option>
+                    <option value="Australia">Australia</option>
+                    <option value="Antarctica">Antarctica</option>
                 </select>
                 <div>
                     <label> Male
                         <input
                             name="sex"
-                            value="male" 
-                            type="radio"/>
+                            value="Male"
+                            onChange={this.handleChange}
+                            type="radio" />
                     </label>
                     <label> Female
                         <input
                             name="sex"
-                            value="female" 
-                            type="radio"/>
+                            value="Female"
+                            onChange={this.handleChange}
+                            type="radio" />
                     </label>
                 </div>
                 <div>
-                    <label> Sport
+                    <label>Sport
                         <input
-                            name="sport" 
-                            type="checkbox"/>
+                            type="checkbox"
+                            value="sport"
+                            onChange={this.handleChange}
+                            checked={this.state.activities.sport} />
                     </label>
-                    <label> Coding
+                    <label>Coding
                         <input
-                            name="coding" 
-                            type="checkbox"/>
+                            type="checkbox"
+                            value="coding"
+                            onChange={this.handleChange}
+                            checked={this.state.activities.coding} />
                     </label>
-                    <label> Videogames
-                        <input 
-                            name="videogames"
-                            type="checkbox"/>
+                    <label>Videogames
+                        <input
+                            type="checkbox"
+                            value="videogames"
+                            onChange={this.handleChange}
+                            checked={this.state.activities.videogames} />
                     </label>
-                    <label> Art
-                        <input 
-                            name="art"
-                            type="checkbox"/>
+                    <label>Art
+                        <input
+                            type="checkbox"
+                            value="art"
+                            onChange={this.handleChange}
+                            checked={this.state.activities.art} />
                     </label>
-                    <label> Music
-                        <input 
-                            name="music"
-                            type="checkbox"/>
+                    <label>Music
+                        <input
+                            type="checkbox"
+                            value="music"
+                            onChange={this.handleChange}
+                            checked={this.state.activities.music} />
                     </label>
                 </div>
                 <div>
                     <label>
-                        <input 
-                            type="date"/>
+                        <input
+                            name="date"
+                            value={this.state.date}
+                            onChange={this.handleChange}
+                            type="date" />
                     </label>
                 </div>
                 <button type="submit">Submit</button>
